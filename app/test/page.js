32acc2c38fa7897f17b143e4b994b72184bfeaf5
Andrew Hall <andrewmartinhall2@gmail.com>
2024-01-05 09:03:22 -0700

    test 6

--- app/test/page.js
+++ app/test/page.js
@@ -4,7 +4,6 @@ export default async function Page(){
   const date = "2023-11-01"
   const test = await getMarketData(date, 'AAPL,IBM')
   const price = test.bars.IBM[0].c
-  console.log(price)
   return(
     <div>
       {price}
