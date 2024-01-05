ac2c0f396aa0f29e4a5a4e4dd84db434d3656725
Andrew Hall <andrewmartinhall2@gmail.com>
2024-01-04 19:09:48 -0700

    Figured out environment variables

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    APCA_API_KEY: 'PKIMSQD9AVGDW5ORBZG9',
-    APCA_API_SECRET: 'nQCZrQbN6UbG9agpfTnc6wsGZkrIJh1SvlS28kL0',
-    SUPABASE_URL: 'https://ybxbzwnrnpounhmfcmah.supabase.co',
-    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlieGJ6d25ybnBvdW5obWZjbWFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI0OTgyNDIsImV4cCI6MjAxODA3NDI0Mn0.K-GT-h5AL2nOi8KzbZF0WClwTxyxyplK37ds9yRzqYc'
-  }
 }
 
 module.exports = nextConfig
