9cf281b885c616560e58ab8bc6a8cd2c09301dfb
Andrew Hall <andrewmartinhall2@gmail.com>
2024-01-05 08:33:26 -0700

    Test 4

--- app/page.js
+++ app/page.js
@@ -2,29 +2,29 @@ import { getPortfolio } from "@/lib/data"
 import { Flex, Table } from "@radix-ui/themes"
 
 export default async function HomePage(){
-  // const rows = await getPortfolio()
+  const rows = await getPortfolio()
 
-  // const headers = [
-  //   'Ticker',
-  //   'Shares',
-  //   'Average Cost',
-  //   'Current Price',
-  //   'Beta',
-  //   'Return'
-  // ]
+  const headers = [
+    'Ticker',
+    'Shares',
+    'Average Cost',
+    'Current Price',
+    'Beta',
+    'Return'
+  ]
 
-  // const data = [
-  //   'ticker',
-  //   'num_shares',
-  //   'average_cost',
-  //   'current_price',
-  //   'beta',
-  //   'return_td'
-  // ]
+  const data = [
+    'ticker',
+    'num_shares',
+    'average_cost',
+    'current_price',
+    'beta',
+    'return_td'
+  ]
 
   return(
     <>
-      {/* <Flex direction='column' align='start' p='4'>
+      <Flex direction='column' align='start' p='4'>
         <Table.Root variant="surface">
           <Table.Header>
             <Table.Row>
@@ -43,7 +43,7 @@ export default async function HomePage(){
             ))}
           </Table.Body>
         </Table.Root>  
-      </Flex> */}
+      </Flex>
     </>
   )
 }
