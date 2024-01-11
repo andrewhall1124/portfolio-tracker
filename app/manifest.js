540ffb0def4ad7dc4089e00a746e010c843e99d1
Andrew Hall <andrewmartinhall2@gmail.com>
2024-01-11 13:14:07 -0700

    Standalone Mobile

---
+++ app/manifest.js
@@ -0,0 +1,17 @@
+export default function manifest() {
+  return {
+    name: 'Portfolio Tracker',
+    short_name: 'Portfolio Tracker',
+    description: 'Made by Andrew Hall',
+    start_url: '/',
+    display: 'standalone',
+    theme_color: '#000000',
+    // icons: [
+    //   {
+    //     src: '/favicon.ico',
+    //     sizes: 'any',
+    //     type: 'image/x-icon',
+    //   },
+    // ],
+  }
+}
